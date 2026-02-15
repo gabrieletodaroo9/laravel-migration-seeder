9f5729cd7d4940f6a8cf94d34a2ce81ba43683da
gabrieletodaroo9 <gabrieletodaroo9@gmail.com>
2026-02-15 22:13:05 +0100

    change query into home function in controller

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class PageController extends Controller
 {
     public function home(){
-        $trains = Train::all();
+        $trains = Train::where('orario_partenza', '>=', date('Y-m-d'))->orderBy('orario_partenza')->get();
         return view("home",compact("trains"));
     }
 }
